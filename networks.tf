96e773b9ffdea441b79a671e9a0d8a6ff45c6677
Julien <sofa.kreutz@yahoo.fr>
2023-02-10 14:47:30 +0100

    Minor fixes

--- networks.tf
+++ networks.tf
@@ -16,13 +16,20 @@ resource "azurerm_virtual_network" "katarina" {
   resource_group_name = azurerm_resource_group.katarina.name
   address_space       = ["13.78.128.0/22"]
   tags = {
-    environment = "Production"
+    environment = "Runeterra"
   }
 }
 
-# Create subnet
-resource "azurerm_subnet" "publicSubnetOne" {
-  name                 = "first-subnet"
+# Create 2 subnets
+resource "azurerm_subnet" "publicSubnetLb" {
+  name                 = "leblanc-subnet"
+  resource_group_name  = azurerm_resource_group.katarina.name
+  virtual_network_name = azurerm_virtual_network.katarina.name
+  address_prefixes     = ["13.78.129.0/24"]
+}
+
+resource "azurerm_subnet" "publicSubnetMf" {
+  name                 = "missfortune-subnet"
   resource_group_name  = azurerm_resource_group.katarina.name
   virtual_network_name = azurerm_virtual_network.katarina.name
   address_prefixes     = ["13.78.129.0/24"]
