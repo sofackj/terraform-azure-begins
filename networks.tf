25e3f424524b89a61a18308ebecd7b4b5a54df4e
Julien <sofa.kreutz@yahoo.fr>
2023-02-10 14:11:11 +0100

    Add providers.tf and few updates

--- networks.tf
+++ networks.tf
@@ -1,32 +1,28 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "katarina" {
+  name     = "katarina-resources"
   location = "West Europe"
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "katarina" {
+  name                = "katarina-security-group"
+  location            = azurerm_resource_group.katarina.location
+  resource_group_name = azurerm_resource_group.katarina.name
 }
-
-resource "azurerm_virtual_network" "example" {
+# Create a virtual network or vpc
+resource "azurerm_virtual_network" "katarina" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-
+  location            = azurerm_resource_group.katarina.location
+  resource_group_name = azurerm_resource_group.katarina.name
+  address_space       = ["13.78.128.0/22"]
   tags = {
     environment = "Production"
   }
+}
+
+# Create subnet
+resource "azurerm_subnet" "publicSubnetOne" {
+  name                 = "first-subnet"
+  resource_group_name  = azurerm_resource_group.katarina.name
+  virtual_network_name = azurerm_virtual_network.katarina.name
+  address_prefixes     = ["13.78.129.0/24"]
 }
