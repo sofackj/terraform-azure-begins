755a23a97ec55ee9df98b866e98c8988cfb161fe
Julien <sofa.kreutz@yahoo.fr>
2023-02-14 09:48:10 +0100

    Add providers.tf and few updates

--- containers.tf
+++ containers.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "poppy" {
   name                = "docker-in-runeterra"
   location            = azurerm_resource_group.katarina.location
   resource_group_name = azurerm_resource_group.katarina.name
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
   os_type             = "Linux"
 
   container {
