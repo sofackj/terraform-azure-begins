85fdc17fe5feda022232e1417be7d580899ee840
Julien <sofa.kreutz@yahoo.fr>
2023-02-14 09:03:10 +0100

    Add providers.tf and few updates

---
+++ containers.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "poppy" {
+  name                = "docker-in-runeterra"
+  location            = azurerm_resource_group.katarina.location
+  resource_group_name = azurerm_resource_group.katarina.name
+  ip_address_type     = "public"
+  os_type             = "Linux"
+
+  container {
+    name   = "kawaii"
+    image  = "nginx:alpine-slim"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "runeterra"
+  }
+}
